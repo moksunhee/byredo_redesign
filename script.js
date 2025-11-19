e2c260e22c09c459fa836fe871ac700f3816c1c8
moksunhee <msh93077@gmail.com>
2025-11-19 10:35:54 +0900

    헤더블러스크립트수정

---
+++ script.js
@@ -0,0 +1,9 @@
+  document.addEventListener("scroll", function () {
+    const header = document.querySelector(".header");
+
+    if (window.scrollY > 80) {
+      header.classList.add("scrolled");
+    } else {
+      header.classList.remove("scrolled");
+    }
+  });
