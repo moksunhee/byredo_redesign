b7f768cd7237ede2479826a4ff3d3ed902e85b16
moksunhee <msh93077@gmail.com>
2025-11-23 00:06:45 +0900

    sec8,중간완성

--- script.js
+++ script.js
@@ -7,3 +7,31 @@
       header.classList.remove("scrolled");
     }
   });
+
+
+//   var swiper = new Swiper('.sec8-swiper', {
+//   loop: true,
+//   spaceBetween: 40,
+//   breakpoints: {
+//     1920: {
+//       slidesPerView: 6,
+//     },
+//     768: {
+//       slidesPerView: 3,
+//     },
+//     480: {
+//       slidesPerView: 2,
+//     },
+//     0: {
+//       slidesPerView: 1,
+//     },
+//   },
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   }
+// });
